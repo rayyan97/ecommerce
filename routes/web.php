14b72859120694c554b33cab8e1a8a71b7879a23
rayyan97 <ray9167086870@gmail.com>
2022-04-14 16:38:50 +0530

    admin lte installed

--- routes/web.php
+++ routes/web.php
@@ -37,9 +37,14 @@ Route::get('shopping-cart', function () {
 Route::get('checkout', function () {
     return view('pages.checkout.index');
 })->name('checkout.index');
-Route::get('admin/dashboard', function () {
-    return view('admin.dashboard')->name('admin.dashboard');
-});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['admin'])->group(function () {
+    Route::get('admin/dashboard', function () {
+        return view('admin.dashboard.dashboard');
+    });
+
+});
